fb6b5099752746f3314e41cc989b948686f412a4
OkYongChoi <ok9623@gmail.com>
2021-05-20 23:52:27 +0900

    First version

---
+++ config_util.py
@@ -0,0 +1,56 @@
+'''
+    Get all of the configuration such as about
+    envs, algos, etc.
+'''
+
+import collections
+from copy import deepcopy
+import yaml
+
+def recursive_dict_update(d, u):
+    """Add all of the items of u into the d"""
+    for k, v in u.items():
+        if isinstance(v, collections.Mapping):
+            d[k] = recursive_dict_update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
+
+
+def get_config(algorithm, minigame):
+    
+    with open("config/envs/sc2_beta.yaml", "r") as f:
+        try:
+            config_envs_dict = yaml.load(f)
+        except yaml.YAMLError as exc:
+            assert False, f"sc2.yaml error: {exc}"
+        env_config = config_envs_dict
+
+    with open("config/default.yaml","r") as f:
+        try:
+            default_config = yaml.load(f)
+        except yaml.YAMLError as exc:
+            assert False, f"default.yaml error: {exc}"
+
+    # The below block depends on each algorithm    
+    with open(f"config/algs/{algorithm}.yaml", "r") as f:
+        try:
+            config_algs_dict = yaml.load(f)
+        except yaml.YAMLError as exc:
+            assert False, f"sc2.yaml error: {exc}"
+        alg_config = config_algs_dict
+
+    final_config_dict = recursive_dict_update(default_config, env_config)
+    final_config_dict = recursive_dict_update(final_config_dict, alg_config)
+
+    final_config_dict['env_args']['map_name'] = minigame
+
+    return final_config_dict
+    
+def config_copy(config):
+    if isinstance(config, dict):
+        return {k: config_copy(v) for k, v in config.items()}
+    elif isinstance(config, list):
+        return [config_copy(v) for v in config]
+    else:
+        return deepcopy(config)
---
+++ learners/__init__.py
@@ -0,0 +1,23 @@
+from .q_learner import QLearner
+from .coma_learner import COMALearner
+from .qtran_learner import QLearner as QTranLearner
+from .LIIR_Learner import LIIRLearner
+from .latent_q_learner import LatentQLearner
+from .rode_learner import RODELearner
+from .Central_V_Learner import CentralV_Learner
+from .noise_q_learner import NoiseQLearner
+from .offpg_learner import OffPGLearner
+from .rode_gat_learner import RODE_GAT_Learner
+
+REGISTRY = {}
+
+REGISTRY["q_learner"] = QLearner
+REGISTRY["coma_learner"] = COMALearner
+REGISTRY["qtran_learner"] = QTranLearner
+REGISTRY["liir_learner"] = LIIRLearner
+REGISTRY['latent_q_learner'] = LatentQLearner
+REGISTRY['rode_learner'] = RODELearner
+REGISTRY['centralV'] = CentralV_Learner
+REGISTRY["noise_q_learner"] = NoiseQLearner
+REGISTRY["offpg_learner"] = OffPGLearner
+REGISTRY['rode_gat_learner'] = RODE_GAT_Learner
