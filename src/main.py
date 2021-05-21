8a044522129610eb27c3fd90f598afb6da8f6e92
OkYongChoi <ok9623@gmail.com>
2021-05-21 19:30:13 +0900

    import run

--- src/main.py
+++ src/main.py
@@ -14,8 +14,7 @@ import numpy as np
 import torch as th
 from utils.logging import get_logger
 import random
-from run import standard_run
-from offpg_run import offpg_run
+from run import standard_run, offpg_run
 import config_util as cu
 
 
@@ -24,15 +23,15 @@ import config_util as cu
 if __name__ == '__main__':
     logger = get_logger()
     
-    algorithm = 'Role_Learning_Agent/qmix_smac_latent'
+    algorithm = 'RNN_Agent/qmix'
     minigame = '2s3z'
 
     # Get all of the configuration such as maps, algorithms, hyper parameters, etc.
     config = cu.config_copy(cu.get_config(algorithm, minigame))
 
     random_Seed = random.randrange(0, 16546)
-    np.random.seed(random_Seed)
-    th.manual_seed(random_Seed)
+    # np.random.seed(random_Seed)
+    # th.manual_seed(random_Seed)
     config['env_args']['seed'] = random_Seed
 
     is_offpg = config['off_pg']
